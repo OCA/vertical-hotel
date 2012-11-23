f56dc56a8362e9521bb81bf138ad5d8f4879a2f7
Nhomar Hernandez <nhomar@gmail.com>
2012-11-22 20:50:17 -0530

    [PATCH 004/140] [FIX] Fixing rst typos to comply with new rst2html tool on list of modules.

--- hotel/__openerp__.py
+++ hotel/__openerp__.py
@@ -20,26 +20,26 @@
 ##############################################################################
 
 {
-    "name" : "Hotel Management",
+    "name" : "Hotel Management Base",
     "version" : "1.0",
     "author" : "Tiny",
     "category" : "Generic Modules/Hotel Management",
     "description": """
-    Module for Hotel/Resort/Property management. You can manage:
-    * Configure Property
-    * Hotel Configuration
-    * Check In, Check out
-    * Manage Folio
-    * Payment
+Module for Hotel/Resort/Rooms/Property management. You can manage:
 
-    Different reports are also provided, mainly for hotel statistics.
+* Configure Property
+* Hotel Configuration
+* Check In, Check out
+* Manage Folio
+* Payment
+
+Different reports are also provided, mainly for hotel statistics.
     """,
     "depends" : ["base","product","sale"],
     "init_xml" : [],
     "demo_xml" : [
     ],
     "update_xml" : [
-
                     "hotel_view.xml",
                     "hotel_data.xml",
                     "hotel_folio_workflow.xml",
