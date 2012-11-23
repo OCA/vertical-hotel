acc420e1aa33f570564b415aaa00f7fb639e91bc
Nhomar Hernandez <nhomar@gmail.com>
2012-11-22 22:44:12 -0530

    [FIX] __openerp__.py with correct rst sintax

--- hotel_reservation/__openerp__.py
+++ hotel_reservation/__openerp__.py
@@ -24,14 +24,17 @@
     "author" : "Tiny",
     "category" : "Generic Modules/Hotel Reservation",
     "description": """
-    Module for Hotel/Resort/Property management. You can manage:
-    * Guest Reservation
-    * Group Reservartion
-      Different reports are also provided, mainly for hotel statistics.
+Module for Hotel/Resort/Property management. You can manage:
+
+* Guest Reservation
+* Group Reservartion
+
+Different reports are also provided, mainly for hotel statistics.
     """,
     "depends" : ["hotel"],
     "init_xml" : [],
-    "demo_xml" : ['hotel_reservation_data.xml',
+    "demo_xml" : [
+        #'hotel_reservation_data.xml',
     ],
     "update_xml" : [
                     "hotel_reservation_view.xml",
