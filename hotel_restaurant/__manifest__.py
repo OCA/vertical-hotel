c5e889089166c8f0bfa1ba30cbb051efce3df440
Krishna Prajapati <krishna.prajapati.serpentcs@gmail.com>
2017-06-07 18:12:03 +0530

    [ADD]Migrate hotel restaurant module as per v10 standard.

--- hotel_restaurant/__manifest__.py
+++ hotel_restaurant/__manifest__.py
@@ -1,54 +1,27 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#    
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# See LICENSE file for full copyright and licensing details.
 
 {
-    "name" : "Hotel Restaurant Management",
-    "version" : "1.0",
-    "author" : "Tiny,Odoo Community Association (OCA)",
-    "category" : "Generic Modules/Hotel Restaurant",
-    "description": """
-    Module for Hotel/Resort/Restaurant management. You can manage:
-    * Configure Property
-    * Restaurant Configuration
-    * table reservation
-    * Generate and process Kitchen Order ticket,
-    * Payment
-
-    Different reports are also provided, mainly for Restaurant.
-    """,
-    "depends" : ["base","hotel"],
-    "init_xml" : [],
-    "demo_xml" : ["hotel_restaurant_data.xml",
+    'name': 'Hotel Restaurant Management',
+    'version': '10.0.1.0.0',
+    'author': 'Serpent Consulting Services Pvt. Ltd., OpenERP SA',
+    'category': 'Generic Modules/Hotel Restaurant',
+    'website': 'http://www.serpentcs.com',
+    'depends': ['hotel'],
+    'license': 'AGPL-3',
+    'demo': [
+        'views/hotel_restaurant_data.xml',
     ],
-    "update_xml" : [
-                    "hotel_restaurant_view.xml",
-                    "report/hotel_restaurant_report.xml",
-                    "hotel_restaurant_workflow.xml",
-                    "wizard/hotel_restaurant_wizard.xml",
-                    "hotel_restaurant_sequence.xml",
-                    "security/ir.model.access.csv",
+    'data': [
+        'security/ir.model.access.csv',
+        'report/hotel_restaurant_report.xml',
+        'wizard/hotel_restaurant_wizard.xml',
+        'views/res_table.xml',
+        'views/kot.xml',
+        'views/bill.xml',
+        'views/folio_order_report.xml',
+        'views/hotel_restaurant_sequence.xml',
+        'views/hotel_restaurant_view.xml',
     ],
-    "active": False,
-    'installable': False
+    'installable': True
 }
-
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
