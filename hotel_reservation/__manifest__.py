a662a18b2782b075b4e48e2259019f3f9c5d11f9
Krishna Prajapati <krishna.prajapati.serpentcs@gmail.com>
2017-06-07 17:39:26 +0530

    [ADD]To add migrated hotel reservation module as per v10 standard.

--- hotel_reservation/__manifest__.py
+++ hotel_reservation/__manifest__.py
@@ -1,51 +1,39 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#    
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# See LICENSE file for full copyright and licensing details.
+
 {
-    "name" : "Hotel Reservation Management",
-    "version" : "1.0",
-    "author" : "Tiny,Odoo Community Association (OCA)",
-    "category" : "Generic Modules/Hotel Reservation",
-    "description": """
-Module for Hotel/Resort/Property management. You can manage:
-
-* Guest Reservation
-* Group Reservartion
-
-Different reports are also provided, mainly for hotel statistics.
-    """,
-    "depends" : ["hotel"],
-    "init_xml" : [],
-    "demo_xml" : [
-        #'hotel_reservation_data.xml',
+    'name': 'Hotel Reservation Management',
+    'version': '10.0.1.0.0',
+    'author': 'Tiny,Odoo Community Association (OCA), Serpent Consulting \
+                Services Pvt. Ltd., OpenERP SA',
+    'category': 'Generic Modules/Hotel Reservation',
+    'description': '''
+                    Module for Hotel/Resort/Property management.
+                    You can manage:
+                    * Guest Reservation
+                    * Group Reservation
+                    Different reports are also provided, mainly for
+                    hotel statistics.
+                    ''',
+    'depends': ['hotel', 'stock', 'mail'],
+    'license': 'AGPL-3',
+    'demo': ['views/hotel_reservation_data.xml', ],
+    'data': [
+        'security/ir.model.access.csv',
+        'wizard/hotel_reservation_wizard.xml',
+        'report/hotel_reservation_report.xml',
+        'views/hotel_reservation_sequence.xml',
+        'views/hotel_reservation_view.xml',
+        'views/hotel_scheduler.xml',
+        'views/report_checkin.xml',
+        'views/report_checkout.xml',
+        'views/max_room.xml',
+        'views/room_res.xml',
+        'views/room_summ_view.xml',
     ],
-    "update_xml" : [
-                    "hotel_reservation_view.xml",
-                    "hotel_reservation_sequence.xml",
-                    "hotel_reservation_workflow.xml",
-                    "wizard/hotel_reservation_wizard.xml",
-                    "report/hotel_reservation_report.xml",
-                    "security/ir.model.access.csv",
-    ],
-    "active": False,
-    'installable': False
+    'js': ['static/src/js/hotel_room_summary.js', ],
+    'qweb': ['static/src/xml/hotel_room_summary.xml'],
+    'css': ['static/src/css/room_summary.css'],
+    'installable': True,
+    'auto_install': False,
 }
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
