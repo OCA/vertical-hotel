1bc4652a1f7f6a6dd52846b04f9de2c9abb33061
Krishna Prajapati <krishna.prajapati.serpentcs@gmail.com>
2017-06-07 18:54:57 +0530

    [PATCH 07/47] [ADD]To add migrated hotel report reservation module.

--- report_hotel_reservation/__manifest__.py
+++ report_hotel_reservation/__manifest__.py
@@ -1,42 +1,18 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
-#    $Id$
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# See LICENSE file for full copyright and licensing details.
 
 {
-    "name" : "Hotel Reservation Management - Reporting",
-    "version" : "1.0",
-    "author" : "Tiny,Odoo Community Association (OCA)",
-    "depends" : ["hotel_reservation"],
-    "category" : "Generic Modules/Hotel Reservation",
-     "description": """
-    Module shows the status of room reservation
-     * Current status of reserved room
-     * List status of room as draft or done state
-    """,
-    "init_xml" : [],
-    "demo_xml" : [],
-    "update_xml" : ["security/ir.model.access.csv","report_hotel_reservation_view.xml"],
-    "active": False,
-    'installable': False
+    'name': 'Hotel Reservation Management - Reporting',
+    'version': '10.0.1.0.0',
+    'author': 'Serpent Consulting Services Pvt. Ltd., OpenERP SA',
+    'website': 'http://www.serpentcs.com',
+    'depends': ['hotel_reservation'],
+    'license': 'AGPL-3',
+    'category': 'Generic Modules/Hotel Reservation',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/report_hotel_reservation_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
 }
-
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
