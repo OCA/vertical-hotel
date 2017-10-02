554610d5a41c99b01a97c0005e8d505d87ffcc9f
Serpent Consulting Services Pvt. Ltd <serpentcs.lp@gmail.com>
2017-10-02 20:02:14 +0530

    Create README.rst

--- hotel/__manifest__.py
+++ hotel/__manifest__.py
@@ -2,33 +2,30 @@
 # See LICENSE file for full copyright and licensing details.
 
 {
-    "name" : "Hotel Management Base",
-    "version" : "10.0.1.0.0",
-    "author" : "Tiny,Odoo Community Association (OCA), Serpent Consulting Services Pvt. Ltd.",
-    "category" : "Generic Modules/Hotel Management",
-    "description": """
-Module for Hotel/Resort/Rooms/Property management. You can manage:
-
-* Configure Property
-* Hotel Configuration
-* Check In, Check out
-* Manage Folio
-* Payment
-
-Different reports are also provided, mainly for hotel statistics.
-    """,
-    "depends" : ["sale"],
-    "init_xml" : [],
-    "demo_xml" : [
-    ],
-    "update_xml" : [
-                    "hotel_view.xml",
-                    "hotel_data.xml",
-                    "hotel_folio_workflow.xml",
-                    "report/hotel_report.xml",
-                    "wizard/hotel_wizard.xml",
-                    "security/hotel_security.xml",
-                    "security/ir.model.access.csv",
+    'name' : 'Hotel Management Base',
+    'version' : '10.0.1.0.0',
+    'author' : 'Odoo Community Association (OCA), Serpent Consulting\
+                Services Pvt. Ltd., Odoo S.A.',
+    'category' : 'Generic Modules/Hotel Management',
+    'description' : '''
+        Module for Hotel/Resort/Rooms/Property management. You can manage:
+            * Configure Property
+            * Hotel Configuration
+            * Check In, Check out
+            * Manage Folio
+            * Payment
+        Different reports are also provided, mainly for hotel statistics.
+    ''',
+    'depends' : ['sale_stock', 'point_of_sale', 'report'],
+    'demo': ['views/hotel_data.xml'],
+    'data': [
+            'security/hotel_security.xml',
+            'security/ir.model.access.csv',
+            'views/hotel_sequence.xml',
+            'views/hotel_report.xml',
+            'views/report_hotel_management.xml',
+            'views/hotel_view.xml',
+            'wizard/hotel_wizard.xml',
     ],
     'installable': True,
     'application': True,
