f6de3b91ec73cbd104a3f2c87bcdefd5a96c6a53
Serpent Consulting Services Pvt. Ltd <serpentcs.lp@gmail.com>
2017-10-02 20:02:14 +0530

    [PATCH 10/47] Create README.rst

--- report_hotel_reservation/__manifest__.py
+++ report_hotel_reservation/__manifest__.py
@@ -4,7 +4,8 @@
 {
     'name': 'Hotel Reservation Management - Reporting',
     'version': '10.0.1.0.0',
-    'author': 'Serpent Consulting Services Pvt. Ltd., ODOO S.A., Odoo Community Association(OCA)',
+    'author' : 'Odoo Community Association (OCA), Serpent Consulting\
+                Services Pvt. Ltd., Odoo S.A.',
     'website': 'http://www.serpentcs.com',
     'depends': ['hotel_reservation'],
     'category': 'Generic Modules/Hotel Reservation',
