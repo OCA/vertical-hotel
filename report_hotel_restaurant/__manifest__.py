e8cb0066b525b71b088f957f0bc95486f436eea9
Serpent Consulting Services Pvt. Ltd <serpentcs.lp@gmail.com>
2017-10-02 17:41:48 +0530

    [PATCH 08/42] Update __manifest__.py

--- report_hotel_restaurant/__manifest__.py
+++ report_hotel_restaurant/__manifest__.py
@@ -4,15 +4,13 @@
 {
     'name': 'Restaurant Management - Reporting',
     'version': '10.0.1.0.0',
-    'author': 'Serpent Consulting Services Pvt. Ltd., OpenERP SA',
-    'website': 'http://www.serpentcs.com, http://www.openerp.com',
+    'author': 'Serpent Consulting Services Pvt. Ltd., ODOO S.A., Odoo Community Association(OCA)',
+    'website': 'http://www.serpentcs.com, http://www.odoo.com',
     'depends': ['hotel_restaurant', 'report_hotel_reservation'],
-    'license': 'AGPL-3',
     'category': 'Generic Modules/Hotel Restaurant',
     'data': [
         'security/ir.model.access.csv',
         'views/report_hotel_restaurant_view.xml',
     ],
     'installable': True,
-    'auto_install': False,
 }
