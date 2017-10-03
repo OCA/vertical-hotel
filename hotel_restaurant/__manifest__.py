b7555dcd3cf2ad5f1733df62dbefc4c84878434f
Jay Vora(SerpentCS) <jay.vora@serpentcs.com>
2017-10-03 13:32:49 +0530

    [REF]

--- hotel_restaurant/__manifest__.py
+++ hotel_restaurant/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Hotel Restaurant Management',
     'version': '10.0.1.0.0',
-    'author' : 'Odoo Community Association (OCA), Serpent Consulting\
+    'author': 'Odoo Community Association (OCA), Serpent Consulting\
                 Services Pvt. Ltd., Odoo S.A.',
     'category': 'Generic Modules/Hotel Restaurant',
     'website': 'http://www.serpentcs.com',
