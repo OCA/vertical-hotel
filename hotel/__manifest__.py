b86a351e66396120f50063dbeabd600ff31d6604
Krishna Prajapati <krishna.prajapati.serpentcs@gmail.com>
2017-10-25 19:11:53 +0530

    Remove description tag in manifest file.

--- hotel/__manifest__.py
+++ hotel/__manifest__.py
@@ -8,17 +8,6 @@
                 Services Pvt. Ltd., Odoo S.A.',
     'category': 'Generic Modules/Hotel Management',
     'license': 'AGPL-3',
-    'description': '''
-        * Module for Hotel/Resort/Rooms/Property management. You can manage:
-            * Configure Property
-            * Hotel Configuration (Room, Amenities, Services, Floor,
-              Currency Exchange)
-            * Book Your Hotel Room
-            * Manage History of Check In, Check out
-            * Manage Folio
-            * Payment
-        * Different Reports are also provided, mainly for hotel statistics
-    ''',
     'depends': ['sale_stock', 'point_of_sale', 'report'],
     'demo': ['views/hotel_data.xml'],
     'data': [
