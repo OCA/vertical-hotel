9cf8cd167c5bc0a816881ebf583da2836ab046ef
Krishna Prajapati <krishna.prajapati.serpentcs@gmail.com>
2017-11-03 17:47:46 +0530

    [IMP]Improved testcases in hotel_housekeeping module.

--- hotel_housekeeping/tests/test_hotel_housekeeping.py
+++ hotel_housekeeping/tests/test_hotel_housekeeping.py
@@ -18,8 +18,12 @@ class TestHousekeeping(common.TransactionCase):
         self.act_type = self.env.\
             ref('hotel_housekeeping.hotel_housekeeping_activity_type_1')
         self.room = self.env.ref('hotel.hotel_room_0')
+        self.activity = self.env.\
+            ref('hotel_housekeeping.hotel_room_activities_1')
 
-        cur_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        cur_date = datetime.now().strftime('%Y-%m-21 %H:%M:%S')
+        cur_date1 = datetime.now().strftime('%Y-%m-23 %H:%M:%S')
+
         self.housekeeping = self.housekeeping_obj.\
             create({'current_date': time.strftime('%Y-%m-%d'),
                     'room_no': self.room.id,
@@ -34,11 +38,28 @@ class TestHousekeeping(common.TransactionCase):
             create({'name': 'Test Room Activity',
                     'activity_id': self.act_type.id,
                     })
+
+        self.hotel_activity = self.hotel_act_obj.\
+            create({'a_list': self.housekeeping.id,
+                    'today_date': time.strftime('%Y-%m-%d'),
+                    'activity_name': self.activity.id,
+                    'housekeeper': self.housekeeper.id,
+                    'clean_start_time': cur_date,
+                    'clean_end_time': cur_date1,
+                    })
+
         self.hotel_act_type.name_get()
         hotel_activity_type = self.hotel_act_type_obj.\
             name_search('Test Room Activity')
         self.assertEqual(len(hotel_activity_type), 1,
                          'Incorrect search number result for name_search')
+
+    def test_activity_check_clean_start_time(self):
+        self.hotel_activity.check_clean_start_time()
+
+    def test_activity_default_get(self):
+        fields = ['room_id', 'today_date']
+        self.hotel_activity.default_get(fields)
 
     def test_action_set_to_dirty(self):
         self.housekeeping.action_set_to_dirty()
