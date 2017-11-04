3d1b273c0fe8319851ebdd341ec409e9466aa194
Krishna Prajapati <krishna.prajapati.serpentcs@gmail.com>
2017-11-04 14:28:27 +0530

    [IMP]Update the code in hotel_restaurant module.

--- hotel_restaurant/models/hotel_restaurant.py
+++ hotel_restaurant/models/hotel_restaurant.py
@@ -499,24 +499,24 @@ class HotelRestaurantOrder(models.Model):
         hsl_obj = self.env['hotel.service.line']
         so_line_obj = self.env['sale.order.line']
         for order_obj in self:
-                hotelfolio = order_obj.folio_id.order_id.id
-                if order_obj.folio_id:
-                    for order1 in order_obj.order_list:
-                        values = {'order_id': hotelfolio,
-                                  'name': order1.name.name,
-                                  'product_id': order1.name.product_id.id,
-                                  'product_uom': order1.name.uom_id.id,
-                                  'product_uom_qty': order1.item_qty,
-                                  'price_unit': order1.item_rate,
-                                  'price_subtotal': order1.price_subtotal,
-                                  }
-                        sol_rec = so_line_obj.create(values)
-                        hsl_obj.create({'folio_id': order_obj.folio_id.id,
-                                        'service_line_id': sol_rec.id})
-                        hf_rec = hotel_folio_obj.browse(order_obj.folio_id.id)
-                        hf_rec.write({'hotel_restaurant_order_ids':
-                                      [(4, order_obj.id)]})
-                self.state = 'done'
+            hotelfolio = order_obj.folio_id.order_id.id
+            if order_obj.folio_id:
+                for order1 in order_obj.order_list:
+                    values = {'order_id': hotelfolio,
+                              'name': order1.name.name,
+                              'product_id': order1.name.product_id.id,
+                              'product_uom': order1.name.uom_id.id,
+                              'product_uom_qty': order1.item_qty,
+                              'price_unit': order1.item_rate,
+                              'price_subtotal': order1.price_subtotal,
+                              }
+                    sol_rec = so_line_obj.create(values)
+                    hsl_obj.create({'folio_id': order_obj.folio_id.id,
+                                    'service_line_id': sol_rec.id})
+                    hf_rec = hotel_folio_obj.browse(order_obj.folio_id.id)
+                    hf_rec.write({'hotel_restaurant_order_ids':
+                                  [(4, order_obj.id)]})
+            self.state = 'done'
         return True
 
 
