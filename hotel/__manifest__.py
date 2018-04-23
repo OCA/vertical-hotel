116a45d87fcc1fce448efbfb84b4a82704fba6a8
KrishnaPrajapati-SerpentCS <krishna.prajapati.serpentcs@gmail.com>
2018-04-23 16:29:17 +0530

    hotel: Migration to 11.0

--- hotel/__manifest__.py
+++ hotel/__manifest__.py
@@ -1,14 +1,13 @@
-# -*- coding: utf-8 -*-
 # See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Hotel Management Base',
-    'version': '10.0.1.0.0',
-    'author': 'Odoo Community Association (OCA), Serpent Consulting\
-                Services Pvt. Ltd., Odoo S.A.',
+    'name': 'Hotel Management',
+    'version': '11.0.1.0.0',
+    'author': 'Serpent Consulting Services Pvt. Ltd., OpenERP SA',
     'category': 'Generic Modules/Hotel Management',
-    'license': 'AGPL-3',
-    'depends': ['sale_stock', 'point_of_sale', 'report'],
+    'website': 'http://www.serpentcs.com',
+    'depends': ['sale_stock', 'point_of_sale'],
+    'license':'AGPL-3',
     'demo': ['views/hotel_data.xml'],
     'data': [
             'security/hotel_security.xml',
@@ -20,6 +19,8 @@
             'wizard/hotel_wizard.xml',
     ],
     'css': ['static/src/css/room_kanban.css'],
+    'images': ['static/description/Hotel.png'],
+    'auto_install': False,
     'installable': True,
-    'application': True,
+    'application': True
 }
