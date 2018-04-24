8ce5fbe5e807e9711fa72d66978b59b9fcea2a90
KrishnaPrajapati-SerpentCS <krishna.prajapati.serpentcs@gmail.com>
2018-04-24 12:35:05 +0530

    [MIG] report_hotel_restaurant: Migration to 11.0

--- report_hotel_restaurant/__manifest__.py
+++ report_hotel_restaurant/__manifest__.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # See LICENSE file for full copyright and licensing details.
 
 {
     'name': 'Restaurant Management - Reporting',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'author': 'Odoo Community Association (OCA), Serpent Consulting\
                 Services Pvt. Ltd., Odoo S.A.',
     'website': 'http://www.serpentcs.com',
