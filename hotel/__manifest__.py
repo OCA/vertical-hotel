a394587f9a2e2be4e7a25d861356ab9176620a83
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2018-09-27 10:45:46 +0530

    [PATCH 042/140] [MIG]Migarted Module v11: Hotel Reservation

--- hotel/__manifest__.py
+++ hotel/__manifest__.py
@@ -5,7 +5,7 @@
     'version': '11.0.1.0.0',
     'author': 'Odoo Community Association (OCA), Serpent Consulting \
                Services Pvt. Ltd., OpenERP SA',
-    'category': 'Generic Modules/Hotel Management',
+    'category': 'Hotel Management',
     'website': 'https://github.com/OCA/vertical-hotel/',
     'depends': ['sale_stock', 'point_of_sale'],
     'license': 'AGPL-3',
