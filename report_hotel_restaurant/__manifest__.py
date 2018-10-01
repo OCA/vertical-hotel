6c98ea6c206cabde2f5b4fd50bb3177e499e90cc
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2018-10-01 12:52:05 +0530

    [PATCH 21/42] [IMP]Improved Manifest

--- report_hotel_restaurant/__manifest__.py
+++ report_hotel_restaurant/__manifest__.py
@@ -5,7 +5,8 @@
     'version': '11.0.1.0.0',
     'author': 'Odoo Community Association (OCA), Serpent Consulting\
                 Services Pvt. Ltd., Odoo S.A.',
-    'website': 'http://www.serpentcs.com',
+    'category': 'Generic Modules/Hotel Management',
+    'website': 'https://github.com/OCA/vertical-hotel',
     'depends': ['hotel_restaurant', 'report_hotel_reservation'],
     'category': 'Generic Modules/Hotel Restaurant',
     'license': 'AGPL-3',
