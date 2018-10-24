f432fa51dd4223237ba82d751e5567d284f52f63
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2018-10-24 14:49:38 +0530

    Update test_hotel_housekeeping.py

--- hotel_housekeeping/tests/test_hotel_housekeeping.py
+++ hotel_housekeeping/tests/test_hotel_housekeeping.py
@@ -9,10 +9,10 @@ class TestHousekeeping(common.TransactionCase):
 
     def setUp(self):
         super(TestHousekeeping, self).setUp()
-        context = self.env.context
-        self.env = self.env(context=context.update({
-            'tracking_disable': True,
-        })
+        self.env = self.env(context=dict(
+            self.env.context,
+            tracking_disable=True,
+        ))
         self.housekeeping_obj = self.env['hotel.housekeeping']
         self.hotel_act_obj = self.env['hotel.housekeeping.activities']
         self.hotel_act_type_obj = self.env['hotel.housekeeping.activity.type']
