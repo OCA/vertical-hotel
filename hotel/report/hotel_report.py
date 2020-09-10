d5de5eab3cd53acfab79bb06313a0789939cbaca
Nikita Vaghela <nikita.vaghela.serpentcs@gmail.com>
2020-09-10 17:49:51 +0530

    [PATCH 063/140] [IMP]hotel:optimised code.

--- hotel/report/hotel_report.py
+++ hotel/report/hotel_report.py
@@ -3,11 +3,9 @@
 import time
 from datetime import datetime
 
-from dateutil import parser
 from dateutil.relativedelta import relativedelta
 
 from odoo import api, fields, models
-from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 
 
 class FolioReport(models.AbstractModel):
@@ -24,18 +22,12 @@ class FolioReport(models.AbstractModel):
         ]
         tids = folio_obj.search(act_domain)
         for data in tids:
-            checkin = data.checkin_date.strftime(
-                DEFAULT_SERVER_DATETIME_FORMAT
-            )
-            checkout = data.checkout_date.strftime(
-                DEFAULT_SERVER_DATETIME_FORMAT
-            )
             data_folio.append(
                 {
                     "name": data.name,
                     "partner": data.partner_id.name,
-                    "checkin": parser.parse(checkin),
-                    "checkout": parser.parse(checkout),
+                    "checkin": data.checkin_date,
+                    "checkout": data.checkout_date,
                     "amount": data.amount_total,
                 }
             )
