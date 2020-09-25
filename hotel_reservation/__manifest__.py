2f1d982e6ee092a8aafb59346c68ca95a8ccb813
dhruvitaSerpentCS <dhruvita.v.serpentcs@gmail.com>
2020-09-25 16:28:32 +0530

    [IMP]Improved manifest file.

--- hotel_reservation/__manifest__.py
+++ hotel_reservation/__manifest__.py
@@ -26,6 +26,6 @@
     ],
     "demo": ["demo/hotel_reservation_data.xml"],
     "qweb": ["static/src/xml/hotel_room_summary.xml"],
-    "external_dependencies": {"python": ["dateutil", "pytz"]},
+    "external_dependencies": {"python": ["dateutil"]},
     "installable": True,
 }
