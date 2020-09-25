fe29cc586ced17bafbcb42877ee69d99d0870f2b
dhruvitaSerpentCS <dhruvita.v.serpentcs@gmail.com>
2020-09-25 16:00:17 +0530

    [IMP]Fixed Travis.

--- hotel_reservation/__manifest__.py
+++ hotel_reservation/__manifest__.py
@@ -26,6 +26,6 @@
         "wizards/hotel_reservation_wizard.xml",
     ],
     "qweb": ["static/src/xml/hotel_room_summary.xml"],
-    "external_dependencies": {"python": ["dateutil"]},
+    "external_dependencies": {"python": ["dateutil", "pytz"]},
     "installable": True,
 }
