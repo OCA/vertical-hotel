22dba38f486a5faf9d39484d16a2ddbb3db3f540
ShwetaSerpentCS <s.goltar.serpentcs@gmail.com>
2023-03-03 12:14:29 +0530

    [FIX] [15.0] hotel: fix license

--- hotel/wizard/__init__.py
+++ hotel/wizard/__init__.py
@@ -1,4 +1,4 @@
-# See LICENSE file for full copyright and licensing details.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import hotel_wizard
 from . import sale_make_invoice_advance
