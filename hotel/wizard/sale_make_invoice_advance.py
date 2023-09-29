635d4f9c6f265fdad5887a582dc0713067f78fce
sachin-scs <sachin.d.serpentcs@gmail.com>
2023-09-29 19:01:11 +0530

    hotel

--- hotel/wizard/sale_make_invoice_advance.py
+++ hotel/wizard/sale_make_invoice_advance.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2022-TODAY Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>).
+# Copyright (C) 2023-TODAY Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
@@ -17,14 +17,13 @@ class SaleAdvancePaymentInv(models.TransientModel):
         default="delivered",
         required=True,
         help="""A standard invoice is issued with all the order lines ready for
-        invoicing, according to their invoicing policy
-        (based on ordered or delivered quantity).""",
+            invoicing, according to their invoicing policy
+            (based on ordered or delivered quantity).""",
     )
 
     def create_invoices(self):
         ctx = self.env.context.copy()
         if self._context.get("active_model") == "hotel.folio":
-
             HotelFolio = self.env["hotel.folio"]
             folio = HotelFolio.browse(self._context.get("active_ids", []))
             folio.room_line_ids.mapped("product_id").write({"isroom": True})
@@ -36,5 +35,12 @@ class SaleAdvancePaymentInv(models.TransientModel):
                 }
             )
         res = super(SaleAdvancePaymentInv, self.with_context(**ctx)).create_invoices()
+        return res
 
+    def default_get(self, fields):
+        res = super().default_get(fields)
+        if self.env.context.get("active_model") == "hotel.folio":
+            active_id = self.env.context.get("active_id")
+            folio_id = self.env["hotel.folio"].browse(active_id)
+            res.update({"sale_order_ids": folio_id.order_id})
         return res
