49944a40936716439373bf04d6b0c73a341a388a
sachin-scs <sachin.d.serpentcs@gmail.com>
2023-10-13 18:34:00 +0530

    [PATCH 46/47] [16.0][FIX] Travis

--- report_hotel_reservation/__manifest__.py
+++ report_hotel_reservation/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright (C) 2022-TODAY Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>).
+# Copyright (C) 2023-TODAY Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Hotel Reservation Management - Reporting",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Odoo Community Association (OCA), Serpent Consulting\
                 Services Pvt. Ltd., Odoo S.A.",
     "website": "https://github.com/OCA/vertical-hotel",
