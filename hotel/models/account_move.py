ea8bca17d538d845f967df524da60e65c606f223
Rajan-SCS <rajan.s.serpentcs@gmail.com>
2024-04-23 13:56:44 +0530

    Migrated hotel module in v17.

--- hotel/models/account_move.py
+++ hotel/models/account_move.py
@@ -1,16 +1,15 @@
-# Copyright (C) 2023-TODAY Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>).
+# Copyright (C) 2024-TODAY Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, models
 
 
 class AccountMove(models.Model):
-
     _inherit = "account.move"
 
     @api.model
     def create(self, vals):
-        res = super(AccountMove, self).create(vals)
+        res = super().create(vals)
         if self._context.get("folio_id"):
             folio = self.env["hotel.folio"].browse(self._context["folio_id"])
             folio.write({"hotel_invoice_id": res.id, "invoice_status": "invoiced"})
