a21bfad7f29622a8bcc8f282d3bc261ff3a38756
Rajan-SCS <rajan.s.serpentcs@gmail.com>
2024-04-23 17:19:55 +0530

    [MIG] Migrated hotel_reservation in v17.

--- hotel_reservation/wizards/hotel_reservation_wizard.py
+++ hotel_reservation/wizards/hotel_reservation_wizard.py
@@ -1,7 +1,8 @@
-# Copyright (C) 2023-TODAY Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>).
+# Copyright (C) 2024-TODAY Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class HotelReservationWizard(models.TransientModel):
@@ -50,6 +51,12 @@ class HotelReservationWizard(models.TransientModel):
         return self.env.ref("hotel_reservation.hotel_maxroom_details").report_action(
             self, data=data
         )
+
+    @api.constrains("date_start", "date_end")
+    def check_date(self):
+        for record in self:
+            if record.date_start > record.date_end:
+                raise ValidationError(_("End date must be Greater than the Start date"))
 
 
 class MakeFolioWizard(models.TransientModel):
