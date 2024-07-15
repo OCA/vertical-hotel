85f562abaa77c8ff2cbcc06033eaa4aae509e98d
pavan-serpentcs <pavan.p.serpentcs@gmail.com>
2024-07-15 12:51:52 +0530

    [IMP] [17.0] hotel_housekeeping: Implement code changes as per review comments

--- hotel_housekeeping/__manifest__.py
+++ hotel_housekeeping/__manifest__.py
@@ -4,8 +4,8 @@
 {
     "name": "Hotel Housekeeping Management",
     "version": "17.0.1.0.0",
-    "author": "Odoo Community Association (OCA), Serpent Consulting \
-               Services Pvt. Ltd., Odoo S.A.",
+    """author": "Odoo Community Association (OCA), Serpent Consulting 
+                    Services Pvt. Ltd., Odoo S.A."""
     "website": "https://github.com/OCA/vertical-hotel",
     "license": "AGPL-3",
     "summary": "Manages Housekeeping Activities and its Process",
