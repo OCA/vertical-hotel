72f36c3eb3cf07d2bcfde451eb88e9e25a50f2a7
utsasv-serpentcs <utsav.m.serpentcs@gmail.com>
2025-02-19 15:18:58 +0530

    [MIG] report_hotel_reservation: Migration to 18.0

--- report_hotel_reservation/__manifest__.py
+++ report_hotel_reservation/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Hotel Reservation Management - Reporting",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Odoo Community Association (OCA), Serpent Consulting\
                 Services Pvt. Ltd., Odoo S.A.",
     "website": "https://github.com/OCA/vertical-hotel",
