8d9abbc3e563e5f1715ab078923e7e15079c1088
Vandan-SCS <vandan.p.serpentcs@gmail.com>
2025-09-11 15:48:05 +0530

    [MIG][18.0] hotel_reservation

--- hotel_reservation/models/hotel_folio.py
+++ hotel_reservation/models/hotel_folio.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2024-TODAY Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class HotelFolio(models.Model):
@@ -35,66 +35,3 @@ class HotelFolio(models.Model):
                 if update_vals:
                     reservations.write(update_vals[0])
         return res
-
-
-class HotelFolioLine(models.Model):
-    _inherit = "hotel.folio.line"
-
-    @api.onchange("checkin_date", "checkout_date")
-    def _onchange_checkin_checkout_dates(self):
-        res = super()._onchange_checkin_checkout_dates()
-        avail_prod_ids = []
-        for room in self.env["hotel.room"].search([]):
-            assigned = False
-            for line in room.room_reservation_line_ids.filtered(
-                lambda x: x.status != "cancel"
-            ):
-                if self.checkin_date and line.check_in and self.checkout_date:
-                    if (self.checkin_date <= line.check_in <= self.checkout_date) or (
-                        self.checkin_date <= line.check_out <= self.checkout_date
-                    ):
-                        assigned = True
-                    elif (line.check_in <= self.checkin_date <= line.check_out) or (
-                        line.check_in <= self.checkout_date <= line.check_out
-                    ):
-                        assigned = True
-            if not assigned:
-                avail_prod_ids.append(room.product_id.id)
-        return res
-
-    def write(self, vals):
-        """
-        Overrides orm write method.
-        @param self: The object pointer
-        @param vals: dictionary of fields value.
-        Update Hotel Room Reservation line history"""
-        reservation_line_obj = self.env["hotel.room.reservation.line"]
-        room_obj = self.env["hotel.room"]
-        prod_id = vals.get("product_id") or self.product_id.id
-        checkin = vals.get("checkin_date") or self.checkin_date
-        checkout = vals.get("checkout_date") or self.checkout_date
-
-        is_reserved = self.is_reserved
-        if prod_id and is_reserved:
-            prod_room = room_obj.search([("product_id", "=", prod_id)], limit=1)
-            if self.product_id and self.checkin_date and self.checkout_date:
-                old_prod_room = room_obj.search(
-                    [("product_id", "=", self.product_id.id)], limit=1
-                )
-                if prod_room and old_prod_room:
-                    # Check for existing room lines.
-                    rm_lines = reservation_line_obj.search(
-                        [
-                            ("room_id", "=", old_prod_room.id),
-                            ("check_in", "=", self.checkin_date),
-                            ("check_out", "=", self.checkout_date),
-                        ]
-                    )
-                    if rm_lines:
-                        rm_line_vals = {
-                            "room_id": prod_room.id,
-                            "check_in": checkin,
-                            "check_out": checkout,
-                        }
-                        rm_lines.write(rm_line_vals)
-        return super().write(vals)
